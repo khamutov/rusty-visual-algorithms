67e18232dba57f5b175ee6356bf7af8dc99a2aa2
khamutov <khamutov@ailabs.ru>
2020-10-22 19:33:55 +0300

    feat: initial commit. Add quicksilver as an game engine

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use quicksilver::{
+    geom::{Rectangle, Vector},
+    graphics::{Color, Graphics},
+    Input, Result, Settings, Window,
+};
+
+fn main() {
+    quicksilver::run(
+        Settings {
+            title: "Square Example",
+            ..Settings::default()
+        },
+        app,
+    );
+}
+
+async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> {
+    // Clear the screen to a blank, white color
+    gfx.clear(Color::WHITE);
+    // Paint a blue square with a red outline in the center of our screen
+    // It should have a top-left of (350, 100) and a size of (150, 100)
+    let rect = Rectangle::new(Vector::new(350.0, 100.0), Vector::new(100.0, 100.0));
+    gfx.fill_rect(&rect, Color::BLUE);
+    gfx.stroke_rect(&rect, Color::RED);
+    // Send the data to be drawn
+    gfx.present(&window)?;
+    loop {
+        while let Some(_) = input.next_event().await {}
+    }
+}
